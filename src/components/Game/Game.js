0574856d958301ed8c9247ac4ae1ea756f2487d5
Travo100 <travo100@gmail.com>
2018-08-14 18:57:30 -0700

    Cleaned up some of the code

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import cards from "../../cards.json";
 
 import Card from "../Card";
@@ -15,15 +15,12 @@ class Game extends Component {
     }
 
     getChihuahua = (id) => {
-
-        // alert(id);
-        if(id === this.state.matchedChihuahua) {
+        if (id === this.state.matchedChihuahua) {
             // if a chihuahua is a match 
             // update the matches state by one 
             // but not the guesses state 
             // if the are also a match pick a new random chihuahua
             let randomId = Math.floor(Math.random() * this.state.chihuahuas.length) + 1;
-            console.log(randomId);
             this.setState({
                 matches: this.state.matches + 1,
                 matchedChihuahua: randomId
@@ -35,24 +32,23 @@ class Game extends Component {
                 guesses: this.state.guesses + 1
             });
         }
-
     }
-    
+
     render() {
         return (
             <div className="container">
-                <Scoreboard 
+                <Scoreboard
                     matches={this.state.matches}
                     guesses={this.state.guesses}
                 />
                 <div className="row">
                     {this.state.chihuahuas.map(chihuahua => (
-                        <Card 
+                        <Card
                             key={chihuahua.id}
-                            id = {chihuahua.id}
-                            name = {chihuahua.name}
-                            image = {chihuahua.image}
-                            getChihuahua = {this.getChihuahua}
+                            id={chihuahua.id}
+                            name={chihuahua.name}
+                            image={chihuahua.image}
+                            getChihuahua={this.getChihuahua}
                         />
                     ))}
                 </div>
